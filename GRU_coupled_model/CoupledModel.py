9c455e78d217d7e18c420c98f967a309ad001fa5
Risaken <isakrockstrom@icloud.com>
2026-05-21 19:36:09 +0200

    Added noise

--- GRU_coupled_model/CoupledModel.py
+++ GRU_coupled_model/CoupledModel.py
@@ -1,6 +1,7 @@
 import pickle
 import torch
 from model_stuff import Predictor
+import numpy as np
 
 
 def _reconstruct_model(params, state_dict):
@@ -20,6 +21,31 @@ def _get_scalers(scaler):
     x_scaler = scalers['x_scaler']
     return x_scaler, y_scaler
 
+def _sample_noise(AMOC, threshold, cov_on, cov_off):
+
+    cov = cov_on if AMOC > threshold else cov_off
+    stds = np.sqrt(np.diag(cov))
+    noise  = np.random.multivariate_normal(mean=np.zeros(3), cov=cov)
+
+    if np.abs(noise[0]) < stds[0]:
+        AMOC_noise = noise[0]
+    else: 
+        AMOC_noise = stds[0] * np.sign(noise[0])
+
+    if np.abs(noise[1]) < stds[1]:
+        SFWF_noise = noise[1] 
+    else: 
+        SFWF_noise = stds[1] * np.sign(noise[1])
+    
+    if np.abs(noise[2]) < stds[2]:
+        PD_200m_noise = noise[2]
+    else: 
+        PD_200m_noise = stds[2] * np.sign(noise[2])
+
+    return AMOC_noise, SFWF_noise, PD_200m_noise        
+        
+
+
 class CoupledModel():
 
     def __init__(self, AMOC_load, SFWF_load, PD_200m_load):
@@ -33,7 +59,7 @@ class CoupledModel():
         self.x_scaler_PD_200m, self.y_scaler_PD_200m = _get_scalers(PD_200m_load['scaler'])
 
 
-    def predict(self, device, X, start_idx, lag, AMOC_features, SFWF_features, PD_200m_features, steps):
+    def predict(self, device, X, start_idx, lag, AMOC_features, SFWF_features, PD_200m_features, steps, threshold, cov_on, cov_off):
 
         X_init_AMOC = X[AMOC_features][start_idx - lag : start_idx]
         X_init_SFWF = X[SFWF_features][start_idx - lag : start_idx]
@@ -71,12 +97,25 @@ class CoupledModel():
         SFWF = [SFWF_init_unscaled]
         PD_200m = [PD_200m_init_unscaled]
 
+        threshold_scaled = (threshold - self.y_scaler_AMOC.mean_[0]) / self.y_scaler_AMOC.scale_[0]
+
         for s in range(steps):
 
             with torch.no_grad():
                 AMOC_s = self.NN_AMOC(X_AMOC_t.unsqueeze(0).to(device)).cpu().numpy()
                 SFWF_s = self.NN_SFWF(X_SFWF_t.unsqueeze(0).to(device)).cpu().numpy()
                 PD_200m_s = self.NN_PD_200m(X_PD_200m_t.unsqueeze(0).to(device)).cpu().numpy()
+
+            AMOC_noise, SFWF_noise, PD_200m_noise = _sample_noise(AMOC_s, threshold_scaled, cov_on, cov_off)
+            
+            AMOC_noise_scaled   = AMOC_noise / self.y_scaler_AMOC.scale_[0]
+            SFWF_noise_scaled   = SFWF_noise / self.y_scaler_SFWF.scale_[0]
+            PD_200m_noise_scaled = PD_200m_noise / self.y_scaler_PD_200m.scale_[0]
+
+            AMOC_s += AMOC_noise_scaled
+            SFWF_s += SFWF_noise_scaled
+            PD_200m_s += PD_200m_noise_scaled
+
          
             AMOC_new = torch.tensor([[SFWF_s.item(), PD_200m_s.item()]], dtype=torch.float32)
             X_AMOC_t = torch.cat([X_AMOC_t[1:], AMOC_new], dim=0) 
